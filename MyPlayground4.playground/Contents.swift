c0fba5f245a0e2e84ebcb66608ffc0ab3e819112
رجاء الجساس <rajaaljassas2009@gmail.com>
2022-08-10 20:06:31 +0300

    cw4

---
+++ MyPlayground4.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var name = "Rajaa"
+var birthyear = 2008
+var age = Int
+var currentyear = 2022
+// Hint : currentyear - birthyear
+
+
+// Hello my name is your name and my age is your age
+
+
+print ("Hello my name is \(name) and my age is \(age)")
+
+// قم بعمل if, وطباعة ما اذا كان يمكنك المشاركة في مبادرة الكويت تبرمج او لا ، حيث ان من شروط المبادرة ان يكون عمر الطالب من ١٤ الى ١٨
+
+
+if age >= 14 || age <= 18
+{
+    print("نعم يمكنك المشاركة في الكويت تبرمج")
+    
+}
+else
+{
+    print("لا يمكنك المشاركة في الكويت تبرمج")
+    
+}
+var textname = "8"
+var number = 4
+var doublenumber : Double
+
+doublenumber =  (Double(textname) + ?? 0) + Double (number)
+
